938ef6b613040bc9c984c551580b42652e6c7ec4
Mohamed Sleem <orcadentallab@gmail.com>
2026-01-14 14:47:02 +0200

    Update: Bump version to 1.2

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -101,7 +101,7 @@ export default function Sidebar() {
                             <span className="font-medium">تسجيل الخروج</span>
                         </button>
                         <div className="text-center mt-2 text-[10px] text-gray-300 dark:text-gray-700 font-mono">
-                            v1.5 (Online)
+                            v1.2 (Online)
                         </div>
                     </div>
                 </div>
