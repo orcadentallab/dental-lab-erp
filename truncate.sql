ea85f57044da6a6161d2df7fc544bda4fa5e68c1
Mohamed Sleem <orcadentallab@gmail.com>
2026-06-27 22:46:43 +0300

    security: remove truncate.sql and gitignore it

--- truncate.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-DECLARE r RECORD;
-BEGIN
-  FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname='public') LOOP
-    EXECUTE format('TRUNCATE TABLE public.%I RESTART IDENTITY CASCADE', r.tablename);
-  END LOOP;
-END
-$$;
